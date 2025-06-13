21babc9999c1651e03b3a18cdc0d9b1c11efea2e
Edem-Kestan <ed.kestan@gmail.com>
2025-06-13 16:45:23 +0300

    added the footer and fixing buttons

--- js/script.js
+++ js/script.js
@@ -4,8 +4,10 @@ window.addEventListener("load", () => {
     const preloader = document.getElementById("preloader");
     setTimeout(() => {
         preloader.classList.add("hide");
-    }, 2500); // теперь задержка 2.5 секунды
+    }, 2500);
 });
+
+
 
 
 
@@ -16,6 +18,6 @@ const input = document.querySelector("#phone");
 const iti = window.intlTelInput(input, {
     initialCountry: "ua",
     separateDialCode: true,
-    autoPlaceholder: "off", // <-- это отключит автозаполнение
+    autoPlaceholder: "off",
     utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.19/js/utils.js"
 });
